3ac1c4929d7528dd55f0bf0adcf6b0b8e0b09eb4
Divyraj <divyarajsinh@dolphinwebsolutions.com>
2024-03-13 19:15:52 +0530

    File URL in ui_form & adding some more fields to widget_optoins.

--- code/Dolphin/CustomForms/Block/Adminhtml/FormSubmittedData.php
+++ code/Dolphin/CustomForms/Block/Adminhtml/FormSubmittedData.php
@@ -2,6 +2,8 @@
 
 namespace Dolphin\CustomForms\Block\Adminhtml;
 
+use Magento\Framework\Filesystem;
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Dolphin\CustomForms\Model\ResourceModel\FormData\CollectionFactory;
@@ -9,13 +11,17 @@ use Dolphin\CustomForms\Model\ResourceModel\FormData\CollectionFactory;
 class FormSubmittedData extends Template
 {
     protected $formDataFactory;
+    protected $filesystem;
+    protected $mediaDirectory;
 
     public function __construct(
         Context $context,
-        CollectionFactory $formDataFactory
+        CollectionFactory $formDataFactory,
+        Filesystem $filesystem
     )
     {
         $this->formDataFactory = $formDataFactory;
+        $this->mediaDirectory = $filesystem->getDirectoryRead(DirectoryList::MEDIA);
         parent::__construct($context);
     }
 
@@ -31,5 +37,18 @@ class FormSubmittedData extends Template
         $collection->addFieldToFilter('id', $currentId);
 
         return $collection;
+    }
+
+    public function getFilePath()
+    {
+        $formData = $this->getCollection()->getFirstItem();
+        $fileName = $formData->getData('uploaded_file');
+
+        if (!empty($fileName)) {
+            $filePath = 'custom_forms_images/uploads/' . $fileName; // Adjust the path as needed
+            return $this->_urlBuilder->getBaseUrl(['_type' => \Magento\Framework\UrlInterface::URL_TYPE_MEDIA]) . $filePath;
+        }
+
+        return false;
     }
 }
